15995b7f140f20e46f1772704cb2c4167a35e89c
Meagen Wall <meagenlynn07@gmail.com>
2018-09-06 17:53:15 -0400

    passportjs added

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,15 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS dtbdTest;
+CREATE DATABASE dtbdTest;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+-- USE DATABASE dtbdTest;
+--     CREATE TABLE vendors (
+--     vendorID INT AUTO_INCREMENT NOT NULL,
+--     vendorType VARCHAR NOT NULL ,
+--     name VARCHAR NOT NULL ,
+--     description VARCHAR,
+--     username VARCHAR NOT NULL ,
+--     password PASSWORD NOT NULL ,
+--     email VARCHAR NOT NULL ,
+--     website VARCHAR NOT NULL ,
+--     zip CHAR(5)
+-- );
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,106 +1,41 @@
-var db = require("../models");
-
-
-
-module.exports = function(app) {
-  // Get all Vendors
-  app.get("/api/vendors", function(req, res) {
-    db.Vendor.findAll({}).then(function(dbVendors) {
-      res.json(dbVendors);
-    });
-  });
-
-
-module.exports = function (app) {
-   // Search functionality
-  app.get("/api/vendors/:category/:search", function(req, res) {
-    let search = req.params.category.search;
-    console.log(search);
-    db.Vendor.findAll({
-      where: {
-        vendorType: req.params.category,
-        [Sequelize.Op.or]: [
-          {name:       {[Sequelize.Op.like]: '%' + search + '%'}},
-          {description: {[Sequelize.Op.like]: '%' + search + '%'}},
-        ]
-      }
-    }).then(function(dbVendors) {
-      res.json(dbVendors);
-    });
-  });
-
-
-  // Create a new Vendor
-  app.post("/api/Vendors", function(req, res) {
-    db.Vendor.create(req.body).then(function(dbVendor) {
-      res.json(dbVendor);
-    });
-  });
-
-  // Delete an Vendor by id
-  app.delete("/api/Vendors/:id", function(req, res) {
-    db.Vendor.destroy({ where: { id: req.params.id } }).then(function(dbVendor) {
-      res.json(dbVendor);
-    });
+const db = require('../models');
+var path = require("path");
 
 module.exports = function (app) {
 
-
- 
- 
-
-  //display all vendors to the page
-  app.get("/api/vendors", function(req, res) {
-    db.Vendor.findAll({}).then(function(dbVendors) {
-      res.json(dbVendors);
-    });
+   // create new vendor
+   app.post('/api/newVendor', (req, res) =>{
+    const newVendor = req.body;
+    Vendors.create(vendor).then(() => res.json({success: true }))
   });
-  //search by vendor type
+  
+  // display all vendors to the page
+  app.get('/api/vendors', (req, res) => {
+    Vendors.findAll().then((vendors) => res.json(vendors));
+  });
+  // search by vendor type
   app.get('/api/results/:vendorType', (req, res) => {
     Vendors.findById(req.params.vendorType).then(vendor => res.json(vendor));
   });
-  //search by vendor price
+  // search by vendor price
   app.get('/api/results/:price', (req, res) => {
     Vendors.findById(req.params.price).then(vendor => res.json(vendor));
   });
-  //create new vendor
-  app.post('/api/newVendor', (req, res) =>{
-    const newVendor = req.body;
-    Vendors.create(vendor).then(() => res.json({success: true }))
 
 
-  });
-  //vendor Profile after log-in
+ 
 
-  });
-  //  app.post("/api/Vendors", function(req, res) {
-  //   db.Vendor.create(req.body).then(function(dbVendor) {
-  //     res.json(dbVendor);
-  //   });
-  // });
-  //vendor Profile 
 
+  // vendor  Profile
   app.get('/api/vendorProfile/:vendorId', (req, res) => {
     Vendors.findById(req.params.vendorId).then(vendor => res.json(vendor));
   });
-  //delete vendor profile
+  // delete vendor profile
   app.delete('/api/vendorProfile/:vendorId', (req, res) =>{
-    Vendors.destroy({ where: {id: req.params.vendorId}})
+    Vendors.destroy({where: {id: req.params.vendorId}})
       .then((affectedRows) => res.json(affectedRows));
   });
-  // app.delete("/api/Vendors/:id", function(req, res) {
-  //   db.Vendor.destroy({ where: { id: req.params.id } }).then(function(dbVendor) {
-  //     res.json(dbVendor);
-  //   });
 };
-
-
-
-
-
-
-
-
 
 // Get all examples
   // app.get("/api/examples", function(req, res) {
