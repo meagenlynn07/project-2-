7225c3b9a46a5c41e5112228f65761e56b932859
Meagen Wall <meagenlynn07@gmail.com>
2018-09-06 19:37:01 -0400

    save

--- models/schema.sql
+++ models/schema.sql
@@ -1,20 +1,14 @@
-<<<<<<< HEAD
 -- DROP DATABASE IF EXISTS dtbdTest;
--- CREATE DATABASE dtbdTest;
+-- CREATE DATABASE dtbdTest;saveco  mmit
 
-USE DATABASE dtbdTest;
-    CREATE TABLE vendors (
-    vendorID INT AUTO_INCREMENT NOT NULL,
-    vendorType VARCHAR NOT NULL ,
-    name VARCHAR NOT NULL ,
-    description VARCHAR,
-    username VARCHAR NOT NULL ,
-    password PASSWORD NOT NULL ,
-    email VARCHAR NOT NULL ,
-    website VARCHAR NOT NULL ,
-    zip CHAR(5)
-);
-=======
-DROP DATABASE IF EXISTS wedding_db;
-CREATE DATABASE wedding_db;
->>>>>>> 8df9d048dc58f9be7a6ba1b46b73490ee322bbe0
+-- USE DATABASE dtbdTest;
+--     CREATE TABLE vendors (
+--     vendorID INT AUTO_INCREMENT NOT NULL,
+--     vendorType VARCHAR NOT NULL ,
+--     name VARCHAR NOT NULL ,
+--     description VARCHAR,
+--     username VARCHAR NOT NULL ,
+--     password PASSWORD NOT NULL ,
+--     email VARCHAR NOT NULL ,
+--     website VARCHAR NOT NULL ,
+--     zip CHAR(
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,58 +1,39 @@
 const db = require('../models');
 var path = require("path");
 
-module.exports = function (app) {
+// module.exports = function (app) {
 
    // create new vendor
-   app.post('/api/newVendor', (req, res) =>{
-    const newVendor = req.body;
-    console.log("newVendor: " ,newVendor);
-    db.Vendor.create(newVendor).then(() => res.json({success: true , fullName:newVendor.fullName }))
-
-  });
-
-   // display all vendors to the page in json type
-   app.get('/api/vendors', (req, res) => {
-    db.Vendor.findAll().then((vendors) => res.json(vendors));
-  });
+  //  app.post('/api/newVendor', (req, res) =>{
+  //   const newVendor = req.body;
+  //   Vendors.create(vendor).then(() => res.json({success: true }))
+  // });
   
-
-    // display vendor Profile by ID
-    app.get('/api/vendorProfile/:vendorId?', (req, res) => {
-      db.Vendor.findById(req.params.vendorId).then(vendor => res.json(vendor));
-    });
-
-    //for login
-    app.post('/api/login' , (req , res) =>{
-      var loginInput = req.body;
-      db.Vendor.findOne({
-        where:{
-          email:loginInput.email,
-          password:loginInput.password
-        }
-      }).then(function(result){
-        res.json({success: true , data:result});
-      })
-    });
-
-
-
-
-
-
-
+  // display all vendors to the page
+  // app.get('/api/vendors', (req, res) => {
+  //   Vendors.findAll().then((vendors) => res.json(vendors));
+  // });
+  // search by vendor type
+  // app.get('/api/results/:vendorType', (req, res) => {
+  //   Vendors.findById(req.params.vendorType).then(vendor => res.json(vendor));
+  // });
   // search by vendor price
-  app.get('/api/results/:price', (req, res) => {
-    Vendors.findById(req.params.price).then(vendor => res.json(vendor));
-  });
+  // app.get('/api/results/:price', (req, res) => {
+  //   Vendors.findById(req.params.price).then(vendor => res.json(vendor));
+  // });
 
+
+
+  // vendor  Profile
+  // app.get('/api/vendorProfile/:vendorId', (req, res) => {
+  //   Vendors.findById(req.params.vendorId).then(vendor => res.json(vendor));
+  // });
   // delete vendor profile
-  app.delete('/api/vendorProfile/:vendorId', (req, res) =>{
-    Vendors.destroy({where: {id: req.params.vendorId}})
-      .then((affectedRows) => res.json(affectedRows));
-  });
-};
-
+//   app.delete('/api/vendorProfile/:vendorId', (req, res) =>{
+//     Vendors.destroy({where: {id: req.params.vendorId}})
+//       .then((affectedRows) => res.json(affectedRows));
+//   });
+// };
 
 // Get all examples
   // app.get("/api/examples", function(req, res) {
