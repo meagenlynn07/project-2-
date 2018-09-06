ab7c326fe200c6e5131447d055c66ba96e78deab
Meagen Wall <meagenlynn07@gmail.com>
2018-09-06 19:26:20 -0400

    minor changes

--- models/schema.sql
+++ models/schema.sql
@@ -1,15 +1,15 @@
-DROP DATABASE IF EXISTS dtbdTest;
-CREATE DATABASE dtbdTest;
+-- DROP DATABASE IF EXISTS dtbdTest;
+-- CREATE DATABASE dtbdTest;
 
--- USE DATABASE dtbdTest;
---     CREATE TABLE vendors (
---     vendorID INT AUTO_INCREMENT NOT NULL,
---     vendorType VARCHAR NOT NULL ,
---     name VARCHAR NOT NULL ,
---     description VARCHAR,
---     username VARCHAR NOT NULL ,
---     password PASSWORD NOT NULL ,
---     email VARCHAR NOT NULL ,
---     website VARCHAR NOT NULL ,
---     zip CHAR(5)
--- );
+USE DATABASE dtbdTest;
+    CREATE TABLE vendors (
+    vendorID INT AUTO_INCREMENT NOT NULL,
+    vendorType VARCHAR NOT NULL ,
+    name VARCHAR NOT NULL ,
+    description VARCHAR,
+    username VARCHAR NOT NULL ,
+    password PASSWORD NOT NULL ,
+    email VARCHAR NOT NULL ,
+    website VARCHAR NOT NULL ,
+    zip CHAR(5)
+);
