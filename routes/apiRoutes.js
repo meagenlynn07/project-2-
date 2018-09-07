4302e30244f6acafd56b2a2461f89991f11518d0
Sydney Hill <sydneyehill@gmail.com>
2018-09-07 10:06:34 -0400

    handlebars changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -35,12 +35,6 @@ module.exports = function (app) {
       })
     });
 
-
-
-
-
-
-
   // search by vendor price
   app.get('/api/results/:price', (req, res) => {
     Vendors.findById(req.params.price).then(vendor => res.json(vendor));
@@ -52,25 +46,3 @@ module.exports = function (app) {
       .then((affectedRows) => res.json(affectedRows));
   });
 };
-
-
-// Get all examples
-  // app.get("/api/examples", function(req, res) {
-  //   db.Example.findAll({}).then(function(dbExamples) {
-  //     res.json(dbExamples);
-  //   });
-  // });
-
-  // // Create a new example
-  // app.post("/api/examples", function(req, res) {
-  //   db.Example.create(req.body).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
-
-  // // Delete an example by id
-  // app.delete("/api/examples/:id", function(req, res) {
-  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
