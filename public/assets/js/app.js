dd35b95f19073a6736a820b5a35b42cf9627716b
Sydney Hill <sydneyehill@gmail.com>
2018-09-08 00:46:53 -0400

    need to add js to expand /unhide results section on submit

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,4 @@
-var imgSrcs = ["../../Images/1.jpg", "../../Images/2.jpg", "../../Images/3.jpg" , "../../Images/4.jpg" , "../../Images/5.jpg"];
+var imgSrcs = ["../../Images/1.jpg", "../../Images/2.jpg", "../../Images/wedding-groom-1600.jpg" , "../../Images/4.jpg" , "../../Images/5.jpg"];
 setInterval(function() {
     $("#changeBack").css("background", "url(" + imgSrcs[imgSrcs.push(imgSrcs.shift())-1] + ")");
     $("#changeBack").css("background-size","100% 100%");
@@ -17,19 +17,77 @@ $(document).ready(function(){
         // console.log("searchInputtttttt: " , searchInput);
         console.log(result);
         // console.log(result.dataInfo[0].vendorType);
-
+        let searchResult;var imgSrcs = ["../../Images/1.jpg", "../../Images/2.jpg", "../../Images/wedding-groom-1600.jpg" , "../../Images/4.jpg" , "../../Images/5.jpg"];
+        setInterval(function() {
+            $("#changeBack").css("background", "url(" + imgSrcs[imgSrcs.push(imgSrcs.shift())-1] + ")");
+            $("#changeBack").css("background-size","100% 100%");
+        }, 5000); // 每5秒自动切换背景图片  
+        
+        
+        
+        $(document).ready(function(){
+        
+            $("#searchSubmit").on("click",function(event){
+                event.preventDefault();
+                var searchInput = $("#searhInput").val().trim();
+                var vendorType = $('#vendorType option:selected').val().toLowerCase();
+                console.log(vendorType + " " + searchInput);
+            $.get("/results/" + vendorType + "/" + searchInput , function(result){
+                // console.log("searchInputtttttt: " , searchInput);
+                console.log(result);
+                // console.log(result.dataInfo[0].vendorType);
+                let results;
+                if(result.success){
+                    // searchResult = true;
+                    console.log("good");
+                    showAllVendor(result.dataInfo[0].vendorType);
+                    results = 'true';
+                    resetSearch();
+                } else{
+                    // searchResult = false;
+                    //alert("we don't have that Vendors");
+                    noResult();
+                    results = 'false';
+                    resetSearch();
+                }
+        
+                }) 
+            });
+        
+            function resetSearch(){
+                $("#searhInput").val("").attr("placeholder", "Search");;
+                $('#vendorType').val("null");
+            }
+            function showAllVendor(vendorType){
+                window.location.replace("/show/" + vendorType);
+            }
+        
+            function noResult(searchInput){
+                $('#searchFail').addClass('show');
+            }
+        
+        
+        
+        });
         if(result.success){
+            // searchResult = true;
             console.log("good");
             showAllVendor(result.dataInfo[0].vendorType);
+            resetSearch();
         } else{
-            alert("we don't have that Vendors");
+            // searchResult = false;
+            //alert("we don't have that Vendors");
             noResult();
+            resetSearch();
         }
 
         }) 
     });
 
-
+    function resetSearch(){
+        $("#searhInput").val("").attr("placeholder", "Search");;
+        $('#vendorType').val("null");
+    }
     function showAllVendor(vendorType){
         window.location.replace("/show/" + vendorType);
     }
@@ -37,7 +95,5 @@ $(document).ready(function(){
     function noResult(searchInput){
         $('#searchFail').addClass('show');
     }
-
-
 
 });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,35 +9,32 @@ function randInt(min, max) {
 };
 
 module.exports = function (app) {
-    
-   
-    app.get('/', (req, res) => {
-      // Vendors.findAll().then((vendors) => res.json(vendors));
-      //res.sendFile(path.join(__dirname, "main.handlebars"));
-      res.render("index",
-      {
-        title: 'Home'
-      });
-    });
-
-  //home
-  app.get("/home", (req, res) => {
-    console.log("index handlebars");
-    res.render('index', {
+  app.get('/', (req, res) => {
+    // Vendors.findAll().then((vendors) => res.json(vendors));
+    //res.sendFile(path.join(__dirname, "main.handlebars"));
+    res.render("index",
+    {
       title: 'Home'
     });
   });
 
+//home
+app.get("/home", (req, res) => {
+  console.log("index handlebars");
+  res.render('index', {
+    title: 'Home'
+  });
+});
 
-  //login
-    app.get("/login", (req, res) => {
-      console.log("login handlebars");
-      res.render('login', {
-        title: 'Login',
-        // stylesheet: 'login',
-        // script: 'login'
-      });
+//login
+  app.get("/login", (req, res) => {
+    console.log("login handlebars");
+    res.render('login', {
+      title: 'Login',
+      // stylesheet: 'login',
+      // script: 'login'
     });
+  });
 
 //register
   app.get("/register", (req, res) => {
@@ -47,136 +44,125 @@ module.exports = function (app) {
       });
   });
 
-//register
-app.get("/register", (req, res) => {
+//gallery
+app.get("/gallery", (req, res) => {
   console.log("gallery handlebars");
   res.render("gallery");
 });
 
+  app.get("/hi", (req, res) => {
+    console.log("HI");
+    res.send("hiii")
+  });
+  
 
-
-    app.get("/hi", (req, res) => {
-      console.log("HI");
-      res.send("hiii")
+  app.get("/vendors/:fullName", (req, res) => {
+    var fullName = req.params.fullName;
+    console.log("this is fullName: ", fullName);
+    db.Vendor.findOne({
+      where: {
+        fullName: fullName
+      }
+    }).then(function (data) {
+      var hbsObject = {
+        title: "vendorInfo",
+        vendorInfo: data
+      };
+      console.log(hbsObject);
+      res.render("vendors", hbsObject);
     });
-    
-
-    app.get("/vendors/:fullName", (req, res) => {
-
-      var fullName = req.params.fullName;
-      console.log("this is fullName: ", fullName);
-      db.Vendor.findOne({
-        where: {
-          fullName: fullName
-        }
-      }).then(function (data) {
-        var hbsObject = {
-          title: "vendorInfo",
-          vendorInfo: data
-        };
-        console.log(hbsObject);
-        res.render("vendors", hbsObject);
-      });
-    });
+  });
 
 
-    //for the pages which has not finished yet
-    app.get('/workingOn', function (req, res) {
-      console.log("================================");
-      res.sendFile(path.join(__dirname, "../public/pages/maintenance.html"));
-    });
-
-
-
+  //for the pages which has not finished yet
+  app.get('/workingOn', function (req, res) {
+    console.log("================================");
+    res.sendFile(path.join(__dirname, "../public/pages/maintenance.html"));
+  });
 
       // search by vendor type
-      app.get("/results/:vendorType/:searchInput", (req, res) => {
- 
-        console.log("=================================");
-        console.log("searchInput: " , req.params.searchInput);
-        var searchTerm = req.params.searchInput;
-        var vendorType = req.params.vendorType;
-        console.log("what is this: " ,typeof(searchTerm));
-        db.Vendor.findAll({
-          where: {
-            vendorType:vendorType,
-            description: {
-             $like: '%searchInput%'
-            }
+ app.get("/results/:vendorType/:searchInput", (req, res) => {
+  console.log("=================================");
+  console.log("searchInput: " , req.params.searchInput);
+  var searchTerm = req.params.searchInput;
+  var vendorType = req.params.vendorType;
+  console.log("what is this: " ,typeof(searchTerm));
+  db.Vendor.findAll({
+    where: {
+      vendorType:vendorType,
+      description: {
+        $like: '%searchInput%'
+      }
+    }
+  }).then(function (data) {
+      console.log(typeof(data) , data);
+      if(data == {} || data == [] || data == "" || data == null){
+        res.json({success:false});
+      }else{
+        res.json({success:true , dataInfo: data});
+      }
+      // console.log("hiiiii: " , data)
+        // res.json({
+        //   success: true,
+        //   data: data
+        // })
+        //  res.redirect()
+      
+        // console.log(searchData);
+        // res.render("search" , searchData);
+        // res.redirect("/results/photographer");
+      }
+    );
+  });
 
-          }
-        }).then(function (data) {
-          console.log(typeof(data) , data);
-          if(data == {} || data == [] || data == "" || data == null){
-            res.json({success:false});
-          }else{
-            res.json({success:true , dataInfo: data});
-          }
-          // console.log("hiiiii: " , data)
-            // res.json({
-            //   success: true,
-            //   data: data
-            // })
-            //  res.redirect()
-         
-            // console.log(searchData);
-            // res.render("search" , searchData);
-            // res.redirect("/results/photographer");
-          }
-        );
-      });
+  //search by vendor type
+  app.get("/show/:vendorType" , function(req , res){
+    console.log("vendorType: " , req.params.vendorType);
+    db.Vendor.findAll({
+      where: {
+        vendorType:req.params.vendorType
+      }
+    }).then(function (data) {
+      var searchData = {
+        title:"search result",
+        searchResult:data
+      };
+      console.log(data);
+    res.render("search" , searchData);
+  })
 
+});
 
-      //search by vendor type
-      app.get("/show/:vendorType" , function(req , res){
-        console.log("vendorType: " , req.params.vendorType);
-        db.Vendor.findAll({
-          where: {
-            vendorType:req.params.vendorType
-          }
-        }).then(function (data) {
-          var searchData = {
-            title:"search result",
-            searchResult:data
-          };
-          console.log(data);
+    // test for search by vendor type
+    app.get("/results/photographers", (req, res) => {
+    // console.log("searchInput: " , req.params.vendorType);
+    var vendorType = "photographer";
+    db.Vendor.findAll({
+      where: {
+        vendorType:vendorType
+      }
+    }).then(function (data) {
+      // console.log("hiiiii: " , data)
+        // res.json({
+        //   success: true,
+        //   data: data
+        // })
+        //  res.redirect()
+        var searchData = {
+          title:"search result",
+          searchResult:data
+        };
+        console.log(searchData);
         res.render("search" , searchData);
-      })
-
-    });
-
-       // test for search by vendor type
-       app.get("/results/photographers", (req, res) => {
-        // console.log("searchInput: " , req.params.vendorType);
-        var vendorType = "photographer";
-        db.Vendor.findAll({
-          where: {
-            vendorType:vendorType
-          }
-        }).then(function (data) {
-          // console.log("hiiiii: " , data)
-            // res.json({
-            //   success: true,
-            //   data: data
-            // })
-            //  res.redirect()
-            var searchData = {
-              title:"search result",
-              searchResult:data
-            };
-            console.log(searchData);
-            res.render("search" , searchData);
-            // res.redirect("/login");
-          }
-        );
-      });
+        // res.redirect("/login");
+      }
+    );
+  });
 
 
-    // Render 404 page for any unmatched routes
-    app.get("*", function (req, res) {
-      res.render("404");
-    });
-
-
-    };
+  // Render 404 page for any unmatched routes
+  app.get("*", function (req, res) {
+    res.render("404");
+  });
+};
 
