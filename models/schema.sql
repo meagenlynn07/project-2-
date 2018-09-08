288414d30827ebecbc9a6adab204145c9bca275c
Kevin Wei <bionew@Bionews-MacBook-Pro.local>
2018-09-08 11:56:36 -0400

    just save

--- models/schema.sql
+++ models/schema.sql
@@ -1,15 +1,15 @@
--- DROP DATABASE IF EXISTS wedding_db;
--- CREATE DATABASE wedding_db;
+DROP DATABASE IF EXISTS wedding_db;
+CREATE DATABASE wedding_db;
 
-USE DATABASE wedding_db;
-    CREATE TABLE vendors (
-    vendorID INT AUTO_INCREMENT NOT NULL,
-    vendorType VARCHAR NOT NULL ,
-    vendorName VARCHAR NOT NULL,
-    name VARCHAR NOT NULL ,
-    description VARCHAR,
-    username VARCHAR NOT NULL ,
-    password PASSWORD NOT NULL ,
-    email VARCHAR NOT NULL ,
-    website VARCHAR NOT NULL ,
-    zip INT NOT NULL)
+-- USE DATABASE wedding_db;
+--     CREATE TABLE vendors (
+--     vendorID INT AUTO_INCREMENT NOT NULL,
+--     vendorType VARCHAR NOT NULL ,
+--     vendorName VARCHAR NOT NULL,
+--     name VARCHAR NOT NULL ,
+--     description VARCHAR,
+--     username VARCHAR NOT NULL ,
+--     password PASSWORD NOT NULL ,
+--     email VARCHAR NOT NULL ,
+--     website VARCHAR NOT NULL ,
+--     zip INT NOT NULL)
